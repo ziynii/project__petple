80277f7f79655b92125eabcd3ddb73e89a30171c
Jiyeon <yeeoni_@naver.com>
2022-04-13 22:18:08 +0900

    🚧 Mark up footerNav component

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ function App() {
       <Routes>
         <Route />
       </Routes>
+
+      <FooterNav />
     </BrowserRouter>
   );
 }
---
+++ src/components/footerNav.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const FooterNav = () => {
+  return (
+    <div className="footer-nav">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <i className="fa-solid fa-house"></i>
+					<h4 className="nav-item-title">홈</h4>
+        </li>
+        <li className="nav-item">
+          <i className="fa-solid fa-paw"></i>
+					<h4 className="nav-item-title">놀이터</h4>
+        </li>
+        <li className="nav-item">
+          <i className="fa-solid fa-user-group"></i>
+					<h4 className="nav-item-title">커뮤니티</h4>
+        </li>
+        <li className="nav-item">
+          <i className="fa-solid fa-user"></i>
+					<h4 className="nav-item-title">마이페이지</h4>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default FooterNav;
