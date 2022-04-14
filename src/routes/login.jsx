464ae3b1fd1bec8af24b20b010b118701b71d93f
Jiyeon <yeeoni_@naver.com>
2022-04-15 02:06:36 +0900

    🚧 Add login function by email

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useRef } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { auth } from '../firebase';
 
-const Login = (props) => {
+const Login = ({ setShowHeaderAndNav }) => {
+  const navigate = useNavigate();
+  const emailRef = useRef();
+  const pwRef = useRef();
+
+  const handleLoginWithEmail = () => {
+    auth.signInWithEmailAndPassword(
+      emailRef.current.value,
+      pwRef.current.value
+    );
+    navigate('/home');
+    setShowHeaderAndNav(true);
+  };
+
   return (
     <div className="login-wrapper login">
       <div className="logo">
@@ -13,14 +27,32 @@ const Login = (props) => {
 
         <form className="login-form">
           <div className="input-wrap">
-            <i class="fa-solid fa-envelope"></i>
-            <input type="email" className="email-input" />
+            <i className="fa-solid fa-envelope"></i>
+            <input
+              type="email"
+              className="email-input"
+              ref={emailRef}
+              placeholder="이메일을 입력하세요"
+            />
           </div>
 
           <div className="input-wrap">
-            <i class="fa-solid fa-lock"></i>
-            <input type="password" className="pw-input" />
+            <i className="fa-solid fa-lock"></i>
+            <input
+              type="password"
+              className="pw-input"
+              ref={pwRef}
+              placeholder="비밀번호를 입력하세요"
+            />
           </div>
+
+          <button
+            type="submit"
+            className="submit-button"
+            onClick={handleLoginWithEmail}
+          >
+            로그인
+          </button>
         </form>
 
         <div className="sns-login">
@@ -28,7 +60,7 @@ const Login = (props) => {
             <i className="fa-brands fa-github"></i>
           </button>
           <button type="button" className="sns-login-button google-login">
-            <i class="fa-brands fa-google"></i>
+            <i className="fa-brands fa-google"></i>
           </button>
         </div>
 
