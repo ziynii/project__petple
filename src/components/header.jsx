dda02c2e607ad45266b856b66cea4d3aa540a4ef
Jiyeon <yeeoni_@naver.com>
2022-04-15 02:07:36 +0900

    🐛 Set to be visible when showHeaderAndNav state is true

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({ showHeaderAndNav }) => {
   return (
-    <header className="header">
-      <div className="logo">
-        <Link to="/">
-          <img src="/logo.svg" alt="petple 로고" />
-        </Link>
-      </div>
+    <>
+      {showHeaderAndNav ? (
+        <header className="header">
+          <h1 className="logo">
+            <Link to="/main">
+              <img src="/logo.svg" alt="petple 로고" />
+            </Link>
+          </h1>
 
-      <button
-        type="button"
-        className="chat-button"
-        aria-label="채팅페이지로 이동"
-      >
-        <i className="fa-regular fa-comments"></i>
-      </button>
-    </header>
+          <button
+            type="button"
+            className="chat-button"
+            aria-label="채팅페이지로 이동"
+          >
+            <i className="fa-regular fa-comments"></i>
+          </button>
+        </header>
+      ) : null}
+    </>
   );
 };
 
