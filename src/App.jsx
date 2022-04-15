8c8d8fc993f4dddb6a640b901b58416bab8e273b
Jiyeon <yeeoni_@naver.com>
2022-04-15 12:15:49 +0900

    🚧 Store logged-in user information in username state

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,41 @@ import './styles/main.scss';
 import Header from './components/header';
 import FooterNav from './components/footerNav';
 import Welcome from './routes/welcome';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Login from './routes/login';
 import Join from './routes/join';
+import Home from './routes/home';
+import FreePosts from './routes/freePosts';
+import Mypage from './routes/mypage';
+import { auth } from './firebase';
 
 function App() {
-  const [showHeaderAndNav, setShowHeaderAndNav] = useState(false);
+  const [showHeaderAndNav, setShowHeaderAndNav] = useState(true);
+  const [username, setUsername] = useState();
+
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      localStorage.setItem('user', JSON.stringify(user));
+      console.log(user);
+    }
+    if (!user) {
+      localStorage.removeItem('user');
+    }
+  });
+
+  useEffect(() => {
+    setUsername(JSON.parse(localStorage.getItem('user')).displayName);
+  }, []);
 
   return (
     <BrowserRouter>
       <Header showHeaderAndNav={showHeaderAndNav} />
 
       <Routes>
-        <Route path="/" element={<Welcome />} />
+        <Route
+          path="/"
+          element={<Welcome setShowHeaderAndNav={setShowHeaderAndNav} />}
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/join" element={<Join />} />
       </Routes>
