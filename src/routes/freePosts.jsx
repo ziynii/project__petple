38af0b55d3a82b2221549954b81c43ef8220e6fe
Jiyeon <yeeoni_@naver.com>
2022-04-15 15:10:25 +0900

    🚧 Add .new-post-button

--- src/routes/freePosts.jsx
+++ src/routes/freePosts.jsx
@@ -4,9 +4,18 @@ import FreePost from '../components/freePost';
 const FreePosts = (props) => {
   return (
     <div className="main-content free">
-      <div className="title-box">
-        <h3 className="title">놀이터</h3>
-        <p>모두가 자유롭게 소통하는 공간입니다</p>
+      <div className="align-box-top">
+        <div className="conent-title">
+          <h3 className="title">놀이터</h3>
+          <p>모두가 자유롭게 소통하는 공간입니다</p>
+        </div>
+        <button
+          type="button"
+          className="new-post-button"
+          aria-label="새 글쓰기"
+        >
+          <i className="fa-solid fa-pen"></i>
+        </button>
       </div>
 
       <ul className="post-list">
