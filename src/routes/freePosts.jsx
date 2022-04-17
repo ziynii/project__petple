ddfdd796f45d5e6629a77267b6bd26fcf63ae75b
Jiyeon <yeeoni_@naver.com>
2022-04-17 13:46:40 +0900

    🚧 Send data from db to freePost component

--- src/routes/freePosts.jsx
+++ src/routes/freePosts.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import FreePost from '../components/freePost';
+import { db } from '../firebase';
 
-const FreePosts = (props) => {
+const FreePosts = ({ user }) => {
+  const [isUpload, setIsUpload] = useState(false);
+  const [posts, setPosts] = useState([]);
+
+  const openUploadModal = () => {
+    setIsUpload(true);
+  };
+
+  useEffect(() => {
+    db.collection('freePosts')
+      .get()
+      .then((result) => {
+        let postsArray = [];
+        result.forEach((doc) => {
+          postsArray.push(doc.data());
+        });
+        setPosts(postsArray);
+      });
+  }, [isUpload]);
+
   return (
     <div className="main-content free">
       <div className="align-box-top">
@@ -13,13 +33,16 @@ const FreePosts = (props) => {
           type="button"
           className="new-post-button"
           aria-label="새 글쓰기"
+          onClick={openUploadModal}
         >
           <i className="fa-solid fa-pen"></i>
         </button>
       </div>
 
       <ul className="post-list">
-        <FreePost />
+        {posts?.map((post, i) => {
+          return <FreePost post={post} key={i} />;
+        })}
       </ul>
     </div>
   );
