0d20320b92ca2409a7fb4f95daf57ccf3800d351
Jiyeon <yeeoni_@naver.com>
2022-04-19 13:58:35 +0900

    🚧 Remove .post-reaction-wrap

--- src/components/postReaction.jsx
+++ src/components/postReaction.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 const PostReaction = ({ post }) => {
   return (
-    <div className="post-reaction-wrap">
-      <div className="post-reaction">
-        <div className="total-like">
-          <i className="fa-solid fa-heart"></i>
-          {post?.likes?.length}개
-        </div>
+    <div className="post-reaction">
+      <div className="total-like">
+        <i className="fa-solid fa-heart"></i>
+        {post?.likes?.length}개
       </div>
 
       <span className="date">{post?.date}</span>
