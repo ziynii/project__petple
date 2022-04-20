1ddcd2ba11e67f814b061341a18d49e123c509ec
Jiyeon <yeeoni_@naver.com>
2022-04-21 01:05:44 +0900

    🚧 Add link to .nav-item

--- src/components/footerNav.jsx
+++ src/components/footerNav.jsx
@@ -1,26 +1,37 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const FooterNav = ({ showHeaderAndNav }) => {
+  const navigate = useNavigate();
+
   return (
     <>
       {showHeaderAndNav ? (
         <div className="footer-nav">
           <ul className="nav-list">
             <li className="nav-item">
-              <i className="fa-solid fa-house"></i>
-              <h4 className="nav-item-title">홈</h4>
+              <Link to="/home">
+                <i className="fa-solid fa-house"></i>
+                <h4 className="nav-item-title">홈</h4>
+              </Link>
             </li>
             <li className="nav-item">
-              <i className="fa-solid fa-paw"></i>
-              <h4 className="nav-item-title">놀이터</h4>
+              <Link to="/free">
+                <i className="fa-solid fa-paw"></i>
+                <h4 className="nav-item-title">놀이터</h4>
+              </Link>
             </li>
             <li className="nav-item">
-              <i className="fa-solid fa-user-group"></i>
-              <h4 className="nav-item-title">커뮤니티</h4>
+              <Link to="/community">
+                <i className="fa-solid fa-user-group"></i>
+                <h4 className="nav-item-title">커뮤니티</h4>
+              </Link>
             </li>
             <li className="nav-item">
-              <i className="fa-solid fa-user"></i>
-              <h4 className="nav-item-title">마이페이지</h4>
+              <Link to="mypage">
+                <i className="fa-solid fa-user"></i>
+                <h4 className="nav-item-title">마이페이지</h4>
+              </Link>
             </li>
           </ul>
         </div>
