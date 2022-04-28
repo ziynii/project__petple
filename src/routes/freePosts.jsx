0c8e8fe7e26a6e2998c21997023ba2dbeea2330c
Jiyeon <yeeoni_@naver.com>
2022-04-28 20:53:03 +0900

    🚧 Sort posts by date

--- src/routes/freePosts.jsx
+++ src/routes/freePosts.jsx
@@ -14,6 +14,7 @@ const FreePosts = ({ user }) => {
 
   useEffect(() => {
     db.collection('freePosts')
+      .orderBy('date','desc')
       .get()
       .then((result) => {
         let postsArray = [];
