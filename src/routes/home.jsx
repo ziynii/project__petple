f640464bbd95ee156ca1f5c0ae7aba39787d5197
Jiyeon <yeeoni_@naver.com>
2022-05-10 00:16:27 +0900

    🚧 Implement .main-list-box

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,10 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Carousel from '../components/carousel';
+import { db } from '../firebase';
 
 const Home = ({ setShowHeaderAndNav, user }) => {
   const navigate = useNavigate();
+  const [postList, setPostList] = useState([]);
+  const [postIdList, setPostIdList] = useState([]);
 
+  useEffect(() => {
+    db.collection('freePosts')
+      .orderBy('date', 'desc')
+      .limit(5)
+      .get()
+      .then((result) => {
+        let postListArray = [];
+        let postIdListArray = [];
+        result.forEach((doc) => {
+          postListArray.push(doc.data());
+          postIdListArray.push(doc.id);
+        });
+        setPostList(postListArray);
+        setPostIdList(postIdListArray);
+      });
+  }, []);
 
   useEffect(() => {
     setShowHeaderAndNav(true);
@@ -19,6 +38,35 @@ const Home = ({ setShowHeaderAndNav, user }) => {
   return (
     <div className="main-content home">
       <Carousel />
+
+      <div className="main-list-box">
+        <h5 className="main-list-title" onClick={() => navigate('/free')}>
+          놀이터 새글 <i className="fa-solid fa-angle-right"></i>
+        </h5>
+
+        <ul className="main-list">
+          {postList.map((post, i) => {
+            return (
+              <li
+                className="main-item"
+                key={postIdList[i]}
+                onClick={() => navigate(`/detail/${postIdList[i]}`)}
+              >
+                <div className="list-align-box">
+                  <p className="content">{post.content}</p>
+                  <span className="likes">
+                    <strong>❤</strong> {post.likes.length}
+                  </span>
+                </div>
+                <div className="list-align-box">
+                  <span className="author">{post.username}</span>
+                  <span className="date">{post.date}</span>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 };
