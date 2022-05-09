e9f05c07a8d5e9c66495c99070e484dc36dac6eb
Jiyeon <yeeoni_@naver.com>
2022-05-09 11:11:45 +0900

    🚧 Add profile update function

--- src/routes/mypageEdit.jsx
+++ src/routes/mypageEdit.jsx
@@ -1,7 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { auth, db, storage } from '../firebase';
 
-const MypageEdit = ({ user, userImage }) => {
-  console.log(user);
+const MypageEdit = ({ user }) => {
+  const [userName, setUserName] = useState(user.displayName);
+  const [userImage, setUserImage] = useState('');
+  const navigate = useNavigate();
+  const [userId, setUserId] = useState('');
+
+  const onChangeName = (event) => {
+    setUserName(event.target.value);
+  };
+
+  const onSubmitImage = (event) => {
+    event.preventDefault();
+    const file = document.querySelector('#image').files[0];
+    const storageRef = storage.ref();
+    const saveImageRef = storageRef.child('image/' + file.name);
+    const uploadImage = saveImageRef.put(file);
+
+    uploadImage.on(
+      'state_change',
+      null,
+      (error) => {
+        console.log('실패사유', error);
+      },
+      () => {
+        uploadImage.snapshot.ref.getDownloadURL().then((url) => {
+          auth.currentUser.updateProfile({
+            photoURL: url,
+          });
+          setUserImage(url);
+        });
+      }
+    );
+  };
+
+  const onSubmitName = (event) => {
+    event.preventDefault();
+    auth.currentUser.updateProfile({
+      displayName: userName,
+    });
+  };
+
+  const onSubmitSave = () => {
+    db.collection('user').doc(userId).update({
+      image: userImage,
+      name: userName,
+    });
+    navigate('/mypage');
+  };
+
+  useEffect(() => {
+    db.collection('user')
+      .where('email', '==', user?.email)
+      .get()
+      .then((result) => {
+        result.forEach((doc) => setUserId(doc.id));
+      });
+  }, []);
 
   return (
     <div className="main-content mypage-edit">
@@ -12,12 +69,16 @@ const MypageEdit = ({ user, userImage }) => {
         <div
           className="preview-image"
           style={{
-            backgroundImage: `url("/imgs/default-image.png")`,
+            backgroundImage: `url(${
+              user?.photoURL == null ? '/imgs/default-image.png' : user.photoURL
+            })`,
           }}
         ></div>
         <form className="image-form">
-          <input type="file" className="image-input" />
-          <button className="submit-button">이미지 변경 확인</button>
+          <input type="file" className="image-input" id="image" />
+          <button className="submit-button" onClick={onSubmitImage}>
+            이미지 변경 확인
+          </button>
         </form>
       </div>
 
@@ -25,12 +86,21 @@ const MypageEdit = ({ user, userImage }) => {
         <p className="edit-title">이름 변경</p>
 
         <form className="name-form">
-          <input type="text" className="name-input" />
-          <button className="submit-button">이름 변경 확인</button>
+          <input
+            type="text"
+            className="name-input"
+            value={userName}
+            onChange={onChangeName}
+          />
+          <button className="submit-button" onClick={onSubmitName}>
+            이름 변경 확인
+          </button>
         </form>
       </div>
 
-			<button className="save-button">변경내용 저장</button>
+      <button className="save-button" onClick={onSubmitSave}>
+        변경내용 저장
+      </button>
     </div>
   );
 };
