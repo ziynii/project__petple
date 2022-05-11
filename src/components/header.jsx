03a9ce4d4b93a193e7122ce4c1a0f3a226bc9991
Jiyeon <yeeoni_@naver.com>
2022-05-11 13:27:09 +0900

    🚧 Remove .chat-button

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -11,14 +11,6 @@ const Header = ({ showHeaderAndNav }) => {
               <img src="/logo.svg" alt="petple 로고" />
             </Link>
           </h1>
-
-          <button
-            type="button"
-            className="chat-button"
-            aria-label="채팅페이지로 이동"
-          >
-            <i className="fa-regular fa-comments"></i>
-          </button>
         </header>
       ) : null}
     </>
